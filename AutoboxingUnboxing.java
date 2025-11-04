8b28c07bb4a3ab8eab30a7cf7a05e22d6d282098
devansh-ce1 <devansh.maheshwari_cs24@gla.ac.in>
2025-11-04 10:32:50 +0530

    [Devansh] Converted primitive double array to ArrayList<Double> and calculated stats

--- AutoboxingUnboxing.java
+++ AutoboxingUnboxing.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class AutoBoxingUnboxing {
+public class AutoboxingUnboxing {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
 
